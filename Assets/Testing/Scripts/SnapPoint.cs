7d07f66559fb755790f2594d14401d2b4d3865a7
Benjamin Sims <asschaps@outlook.com>
2018-06-02 13:59:42 -0500

    Fixing Project Structure

---
+++ Assets/Testing/Scripts/SnapPoint.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Scripts {
+	public class SnapPoint : MonoBehaviour {
+		public GameObject ConnectedObject = null;
+	
+        public static void Snap (GameObject gameObject) {
+            
+        }
+	}
+}
